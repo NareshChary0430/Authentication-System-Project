e0653203b7ebc3450f5a79a634bed07ec9dfaed4
NareshChary0430 <nareshchari300@gmail.com>
2026-02-13 23:59:46 +0530

    worked on registration page

--- FRONTEND/src/pages/Registration/Registration.jsx
+++ FRONTEND/src/pages/Registration/Registration.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 const Registration = () => {
   return (
-    <div>
-      <h1>Sign up Page</h1>
+    <div style={{height:"100vh"}}>
+      <div  className='signup-left-half w-50 border border-1 border-primary' style={{maxHeight:"100%",overflow:"hidden"}}>
+        <img src="/auth-cover.jpg" alt="auth-system photo" style={{width:"100%"}} />
+      </div>
+      <div  className='signup-right-half w-50'></div>
     </div>
   )
 }
