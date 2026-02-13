e1a2929abeb5d5765d3eb4d32466d063c917291a
NareshChary0430 <nareshchari300@gmail.com>
2026-02-13 23:38:55 +0530

    setup the react project

---
+++ FRONTEND/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Signup from "./pages/Registration/Registration"
+const App = () => {
+  return (
+    <div>
+     <Signup/>
+    </div>
+  )
+}
+
+export default App
