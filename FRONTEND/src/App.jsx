c64154c30288138429f09ffb71b1a25e4a3e4bdc
NareshChary0430 <nareshchari300@gmail.com>
2026-02-15 00:16:39 +0530

    worked on Login page

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Signup from './pages/Registration/Registration'
+import Login from './pages/Login/Login'
 
 function App() {
 
   return (
     <>
       <Signup />
+      <Login />
     </>
   )
 }
---
+++ FRONTEND/src/pages/Login/Login.jsx
@@ -0,0 +1,53 @@
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import './Login.css'
+import { useState } from 'react';
+import axiosClient from '../../utils/axiosClient';
+
+export default function Login() {
+    const [userDetails, setUserDetails] = useState({
+        email: null,
+        password: null
+    })
+
+    function updateFieldData(fieldName, newValue) {
+        setUserDetails(prevDetails => ({
+            ...prevDetails,
+            [fieldName]: newValue
+        })
+        )
+    }
+
+
+    async function submitUserDetails() {
+        let response=await axiosClient.post('/user-login',userDetails);
+    }
+    
+    return <div className='d-flex' style={{ height: '100vh' }}>
+        <div className="signup-left-half w-50" style={{ maxHeight: '100%', overflow: 'hidden' }}>
+            <img src="/auth-cover.jpg" alt="authentication cover" style={{ width: '100%' }} />
+        </div>
+        <div className="signup-right-half w-50 border border-1 border-danger d-flex align-items-center justify-content-center">
+            <div className='w-75'>
+                <h1>Login</h1>
+                <Form>
+                    
+
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Control type="email" placeholder="Enter email" className='form-field' value={userDetails.email} onChange={(e)=>{updateFieldData('email',e.target.value)}}/>
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Control type="password" placeholder="Password" className='form-field' value={userDetails.password} onChange={(e)=>{updateFieldData('password',e.target.value)}}/>
+                    </Form.Group>
+                    <Button variant="primary" type="button" className='w-100' onClick={() => { submitUserDetails() }} >
+                        Login
+                    </Button>
+                </Form>
+                <p>
+                    Dont have an Account? <a href='#' style={{ textDecoration: 'underline' }}> Signup</a>
+                </p>
+            </div>
+        </div>
+    </div>
+}
